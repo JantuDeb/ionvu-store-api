6f9564f9edcab14c26ea3d91c75eb557141bdb4f
Jantu Deb <jantud54@gmail.com>
2022-03-24 13:44:20 +0530

    fix: cookie options

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -3,6 +3,8 @@ const cookieToken = (user) => {
   const options = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
   return { token, options };
 };
