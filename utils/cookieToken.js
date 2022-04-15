dba295352b8e9726c5ea1c24b4259686ab1bac1a
Jantu Deb <63020886+JantuDeb@users.noreply.github.com>
2022-04-16 02:28:07 +0530

    Updated cookie option

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -2,7 +2,9 @@ const cookieToken = (user) => {
   const token = user.getJwtToken();
   const options = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
-    httpOnly: true
+    httpOnly: true,
+secure:true,
+samesite:"none"
   };
   return { token, options };
 };
